fc2d581ec9e8eb24c48957d872d063a09f1ea39e
Mohsen Kazemizade <e.m.kazemizade@gmail.com>
2021-08-20 00:30:46 +0430

    posts models

---
+++ models/posts/BlogPost.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const BlogPostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'adminUser',
+  },
+  date: {
+    type: Date,
+    default: Dater.now,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  pictureUrl: {
+    type: String,
+  },
+  comments: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'comment',
+    },
+  ],
+  likes: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'customerUser',
+    },
+  ],
+});
+module.exports = mongoose.model('blogPost', BlogPostSchema);
---
+++ models/posts/EventPost.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const EventPostSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'adminUser',
+  },
+  date: {
+    type: Date,
+    default: Dater.now,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  pictureUrl: {
+    type: String,
+  },
+  comments: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'comment',
+    },
+  ],
+  likes: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'customerUser',
+    },
+  ],
+  customers: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'customerUser',
+    },
+  ],
+});
+module.exports = mongoose.model('eventPost', EventPostSchema);
